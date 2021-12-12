c3d8dedbfed94ba162cf57ac4b54cb7f3ed4500c
Andres Sierra <andres_sebastian2009@hotmail.com>
2021-12-12 02:20:51 -0500

    Agregar archivos de proyecto.

---
+++ Magneto.AzureFunctions.Tests.ValidatorTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+using Magneto.AzureFunctions.Validator;
+using System;
+using Xunit;
+
+namespace Magneto.AzureFunctions.Tests.ValidatorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            bool result = false;
+            for (int i = 0; i < 1000000; i++)
+            {
+                Random random = new Random();
+                DnaDto dnaDto = new DnaDto();
+                dnaDto = GenerateHuman();
+                char[] letters = { 'A', 'C', 'T', 'G' };
+                var secuenceMin = random.Next(1, 8);
+                var SecuenceLetters = random.Next(2, 10);
+                try
+                {
+                    result = Function.IsMutant(dnaDto, letters, secuenceMin, SecuenceLetters);
+                }
+                catch (Exception)
+                {
+                    Assert.True(result);
+                }
+            }
+            result = true;
+            Assert.True(result);
+        }
+        public DnaDto GenerateHuman()
+        {
+            char[] letters = { 'A', 'C', 'T', 'G' };
+            Random random = new Random();
+            var rowsArray = random.Next(2, 10);
+            var columsArray = random.Next(1, 10);
+            DnaDto dnaDto = new DnaDto();
+            dnaDto.dna = new string[rowsArray];
+            for (int r = 0; r < rowsArray; r++)
+            {
+                string value = "";
+                for (int i = 0; i < columsArray; i++)
+                {
+                    value = value + letters[random.Next(0, letters.Length - 1)];
+                }
+                dnaDto.dna[r] = value;
+            }
+
+            return dnaDto;
+        }
+    }
+}
---
+++ PruebaNet6/Function.cs
@@ -0,0 +1,133 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Net.Http;
+
+namespace Magneto.AzureFunctions.Validator
+{
+    public static class Function
+    {
+        [FunctionName("Validator")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+                string requestbody = await new StreamReader(req.Body).ReadToEndAsync();
+                DnaDto data = JsonConvert.DeserializeObject<DnaDto>(requestbody);
+                char[] dnac = Environment.GetEnvironmentVariable("Letters").ToArray();
+                int secuenceMin = int.Parse(Environment.GetEnvironmentVariable("SecuenceMin"));
+                int secuenceLetters = int.Parse(Environment.GetEnvironmentVariable("SecuenceLetters"));
+                bool res = IsMutant(data, dnac, secuenceMin, secuenceLetters);
+
+                return (res ? new HttpResponseMessage(System.Net.HttpStatusCode.OK) : new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden));
+
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+
+                return new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+            }
+        }
+        public static bool IsMutant(DnaDto data, char[] dnac, int secuenceMin, int secuenceLetters)
+        {
+            List<char> chars = data.dna.SelectMany(x => x).ToList();
+            int counChat = data.dna[0].Count(); int countLetter = dnac.Count(); int secuence = 0;
+            for (int r = 0; (r < countLetter && secuence < secuenceMin); r++)
+            {
+                char letter = dnac[r];
+                int count = chars.Count(x => x == letter);
+                for (int i = 0; (i < count && secuence < secuenceMin); i++)
+                {
+                    int position = chars.IndexOf(letter);
+                    secuence += ValidateSecuenceRight(chars, letter, position, counChat, secuenceLetters);
+                    secuence += ValidateSecuenceBelow(chars, letter, position, counChat, secuenceLetters);
+                    secuence += ValidateSecuenceBelowRight(chars, letter, position, counChat, secuenceLetters);
+                    secuence += ValidateSecuenceBelowLeft(chars, letter, position, counChat, secuenceLetters);
+                    chars[position] = '1';
+                }
+            }
+
+            return (secuence < secuenceMin ? false : true);
+        }
+        private static int ValidateSecuenceRight(List<char> list, char letter, int index, int countChar, int secuenceLetters)
+        {
+            var availablePositions = countChar - index;
+            if (availablePositions >= (secuenceLetters))
+            {
+                if (list.Skip(index).Take(secuenceLetters).Count(x => x == letter) == secuenceLetters)
+                    return 1;
+            }
+            return 0;
+        }
+        private static int ValidateSecuenceBelow(List<char> list, char letter, int index, int countChar, int secuenceLetters)
+        {
+            int rows = (list.Count() / countChar) - (secuenceLetters - 1);
+            int positionMax = rows * countChar;
+            if (index < positionMax)
+            {
+                int sum = 0;
+                for (int i = 1; i < secuenceLetters; i++)
+                {
+                    if (list[index + (countChar * i)].Equals(letter))
+                        sum++;
+                    else
+                        return 0;
+                }
+                if (sum == secuenceLetters - 1)
+                    return 1;
+            }
+            return 0;
+        }
+        private static int ValidateSecuenceBelowRight(List<char> list, char letter, int index, int countChar, int secuenceLetters)
+        {
+            var availablePositions = countChar - index;
+            int rows = (list.Count() / countChar) - (secuenceLetters - 1);
+            int positionMax = rows * countChar;
+            if (availablePositions >= secuenceLetters && index < positionMax)
+            {
+                int sum = 0;
+                for (int i = 1; i < secuenceLetters; i++)
+                {
+                    if (list[index + (countChar * i) + i].Equals(letter))
+                        sum++;
+                }
+                if (sum == secuenceLetters - 1)
+                    return 1;
+                else
+                    return 0;
+            }
+            return 0;
+        }
+        private static int ValidateSecuenceBelowLeft(List<char> list, char letter, int index, int countChar, int secuenceLetters)
+        {
+            var availablePositions = index;
+            int rows = (list.Count() / countChar) - (secuenceLetters - 1);
+            int positionMax = rows * countChar;
+            if (availablePositions <= (secuenceLetters - 1) && index < positionMax)
+            { int sum = 0;
+                    for (int i = 1; i < secuenceLetters; i++)
+                    {
+                        if (list[index + (countChar * i) - i].Equals(letter))
+                            sum++;
+                    }
+                    if (sum == secuenceLetters - 1)
+                        return 1;
+                    else
+                        return 0;
+            }
+
+            return 0;
+        }
+    }
+}
